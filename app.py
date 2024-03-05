a09d4b4a6cbeff9b34d62944d0387c4551f2b9ae
Nanda Budi <nandabudi2003@gmail.com>
2024-03-05 10:55:24 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,9 @@
+
+# Hiung luas segitiga sederhana
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("luas segitiga adalah: ", luas)
+
+luas_segitiga()
